9f0242cae01907347f0c358ce3b28cb3b8b5c0fd
FATIHAS1905 <32196738+FATIHAS1905@users.noreply.github.com>
2019-03-25 18:59:14 +0300

    Create libraryhas.h

---
+++ libraryhas.h
@@ -0,0 +1,8 @@
+#ifndef _library_h_
+#define _library_H_
+void hanoi(int n, char key, char hedef, char ara);
+int fibonacci(int n);
+int fact(int n);
+double per(int x, int y);
+double kom(int x, int y);
+#endif
